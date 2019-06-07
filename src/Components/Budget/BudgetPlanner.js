62b13fac898e06020b81ca5ed0df94d95e1e6c9a
Matthew Bodily <mattcbodily@gmail.com>
2019-06-06 23:54:31 -0600

    styling for the budget planner mostly finished

--- src/Components/Budget/BudgetPlanner.js
+++ src/Components/Budget/BudgetPlanner.js
@@ -2,6 +2,11 @@ import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Doughnut} from 'react-chartjs-2';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faShoppingBag, faGasPump, faTicketAlt, faWineGlassAlt, faCoins} from '@fortawesome/free-solid-svg-icons';
+import {H4, H5, DollarSign, Input, Button, BudgetInfo, Section, ExpenseSplits, ChartContainer} from './BudgetStyles';
+library.add(faShoppingBag, faGasPump, faTicketAlt, faWineGlassAlt, faCoins)
 
 const BudgetPlanner = (props) => {
     const [user, setUser] = useState({})
@@ -58,46 +63,63 @@ const BudgetPlanner = (props) => {
             case 1:
                 return (
                     <div>
-                        <h5>Step One: Set Your Budget</h5>
-                        <input 
-                            value={budget}
-                            onChange={e => setBudget(e.target.value)}/>
-                        <button onClick={incrementStep}>Submit</button>
+                        <H5>Step One: Set Your Budget</H5>
+                        <DollarSign>$
+                            <Input 
+                                value={budget}
+                                onChange={e => setBudget(e.target.value)}/>
+                        </DollarSign>
+                        <Button onClick={incrementStep}>Submit</Button>
                     </div>
                 )
             case 2:
                 return (
                     <div>
-                        <h5>Step Two: Split Your Budget</h5>
-                        <p>Your budget: ${budget}</p>
-                        <p>Budget left: ${(budget - groceries - gas - entertainment - restaurants - other)}</p>
-                        <p>Groceries</p>
-                        <input 
-                            value={groceries}
-                            maxLength='20'
-                            onChange={e => setGroceries(e.target.value)}/>
-                        <p>Gas</p>
-                        <input 
-                            value={gas}
-                            maxLength='20'
-                            onChange={e => setGas(e.target.value)}/>
-                        <p>Entertainment</p>
-                        <input 
-                            value={entertainment}
-                            maxLength='20'
-                            onChange={e => setEntertainment(e.target.value)}/>
-                        <p>Restaurants</p>
-                        <input 
-                            value={restaurants}
-                            maxLength='20'
-                            onChange={e => setRestaurants(e.target.value)}/>
-                        <p>Other</p>
-                        <input 
-                            value={other}
-                            maxLength='20'
-                            onChange={e => setOther(e.target.value)}/>
-                        <button onClick={decrementStep}>Back</button>
-                        <Link to='budget'><button onClick={addMonthlyBudget}>Submit</button></Link>
+                        <H5>Step Two: Split Your Budget</H5>
+                        <BudgetInfo>Your budget: ${budget}</BudgetInfo>
+                        <BudgetInfo>Budget left: ${(budget - groceries - gas - entertainment - restaurants - other)}</BudgetInfo>
+                        <Section>
+                            <FontAwesomeIcon icon='shopping-bag' style={{fontSize: '18px', color: '#00E5C5'}}/>
+                            <ExpenseSplits>Groceries</ExpenseSplits>
+                            <Input 
+                                value={groceries}
+                                maxLength='20'
+                                onChange={e => setGroceries(e.target.value)}/>
+                        </Section>
+                        <Section>
+                            <FontAwesomeIcon icon='gas-pump' style={{fontSize: '18px', color: '#00E55B'}}/>
+                            <ExpenseSplits>Gas</ExpenseSplits>
+                            <Input 
+                                value={gas}
+                                maxLength='20'
+                                onChange={e => setGas(e.target.value)}/>
+                        </Section>
+                        <Section>
+                        <FontAwesomeIcon icon='ticket-alt' style={{fontSize: '18px', color: '#FFAB12'}}/>
+                            <ExpenseSplits>Entertainment</ExpenseSplits>
+                            <Input 
+                                value={entertainment}
+                                maxLength='20'
+                                onChange={e => setEntertainment(e.target.value)}/>
+                        </Section>
+                        <Section>
+                        <FontAwesomeIcon icon='wine-glass-alt' style={{fontSize: '18px', color: '#FF247B'}}/>
+                            <ExpenseSplits>Restaurants</ExpenseSplits>
+                            <Input 
+                                value={restaurants}
+                                maxLength='20'
+                                onChange={e => setRestaurants(e.target.value)}/>
+                        </Section>
+                        <Section>
+                            <FontAwesomeIcon icon='coins' style={{fontSize: '18px', color: '#6600E4'}}/>
+                            <ExpenseSplits>Other</ExpenseSplits>
+                            <Input 
+                                value={other}
+                                maxLength='20'
+                                onChange={e => setOther(e.target.value)}/>
+                        </Section>
+                        <Button onClick={decrementStep}>Back</Button>
+                        <Link to='budget'><Button onClick={addMonthlyBudget}>Submit</Button></Link>
                     </div>
                 )
             default:
@@ -107,24 +129,24 @@ const BudgetPlanner = (props) => {
     const budgetRemaining = (budget - groceries - gas - entertainment - restaurants - other);
     return (
         <div>
-            <h3>Plan your Budget</h3>
-            <div>
+            <H4>Plan your Budget</H4>
+            <ChartContainer>
                 <Doughnut 
-                    height={100}
-                    width={100}
+                    height={200}
+                    width={200}
                     data={{
-                        labels: ['Remaining Budget', 'Groceries', 'Gas', 'Entertainment', 'Restaurants', 'Other'],
                         datasets: [{
-                            backgroundColor: ['#616161', '#FF4242', '#49D4D6', '#8749D6', '#FFC264', '#2CDE00'],
+                            backgroundColor: ['#46AEFF', '#00E5C5', '#00E55B', '#FFAB12', '#FF247B', '#6600E4'],
                             borderColor: '#000000',
                             data: [parseInt(budgetRemaining), parseInt(groceries), parseInt(gas), parseInt(entertainment), parseInt(restaurants), parseInt(other)]
                         }]    
                     }}
                     options={{
                         responsive: true,
-                        maintainAspectRatio: false
+                        maintainAspectRatio: false,
+                        cutoutPercentage: 58
                     }} />
-            </div>
+            </ChartContainer>
             {showStep()}
         </div>
     )
---
+++ src/Components/Budget/BudgetStyles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const H4 = styled.h4`
+    font-size: 25px;
+    font-weight: 300;
+    margin: 25px 0px;
+`
+
+export const H5 = styled.h5`
+    font-size: 18px;
+    font-weight: 300;
+`
+
+export const BudgetInfo = styled.p`
+    font-size: 15px;
+    font-weight: 300;
+`
+
+export const Section = styled.section`
+    text-align: left;
+`
+
+export const ExpenseSplits = styled.span`
+    width: 100px;
+    font-size: 15px;
+    font-weight: 300;
+    margin: 0px;
+`
+
+export const DollarSign = styled.p`
+    font-size: 18px;
+    font-weight: 300;
+`
+
+export const Input = styled.input`
+    height: 45px;
+    width: 200px;
+    border: none;
+    border-bottom: 1px solid lightgray;
+    box-sizing: border-box;
+    font-size: 18px;
+    padding-left: 5px;
+    outline: none;
+`
+
+export const Button = styled.button`
+    height: 40px;
+    width: 220px;
+    background-image:linear-gradient(to right, #8AC1FF, #0078FF);
+    color: white;
+    font-size: 17px;
+    border: none;
+    margin-top: 0px;
+    border-radius: 30px;
+`
+
+export const ChartContainer = styled.div`
+    margin-top: 25px;
+`
