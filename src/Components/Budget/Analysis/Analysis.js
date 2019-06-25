e9a58155001918b60b221b4147216c6324dd0223
Matthew Bodily <mattcbodily@gmail.com>
2019-06-25 09:22:41 -0600

    added endpoints to the anaylsis component to get the data I need

--- src/Components/Budget/Analysis/Analysis.js
+++ src/Components/Budget/Analysis/Analysis.js
@@ -1,9 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Analysis = (props) => {
     const [user, setUser] = useState({})
     const [budget, setBudget] = useState([])
     const [expenses, setExpenses] = useState([])
+
+    useEffect(() => {
+        handleSessionUser()
+    }, [])
+
+    const handleSessionUser = () => {
+        axios.get('/auth/session-user')
+        .then(res => {
+            setUser(res.data)
+            axios.get(`/api/monthly-budget/${res.data.user_id}`)
+            .then(res => {
+                setBudget(res.data)
+                axios.get(`/api/expenses/${res.data[0].user_id}`)
+                .then(res => {
+                    setExpenses(res.data)
+                })
+            })
+        })
+    }
+
+    console.log(user)
+    console.log(budget)
+    console.log(expenses)
 
     return(
         <div>Analysis Component</div>
